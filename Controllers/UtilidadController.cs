a9459dab6eff6682d970f856e196659d704f91cd
gaitan19 <kenleyjos619@gmail.com>
2025-01-08 18:22:16 -0600

    feat: Add print-js library and update ticket styles for better printing

--- Controllers/UtilidadController.cs
+++ Controllers/UtilidadController.cs
@@ -55,13 +55,29 @@ namespace ReactVentas.Controllers
                     .ToString();
 
                 // Get the top 4 sold products in the last 30 days.
+                //config.ProductosVendidos = (from p in _context.Productos
+                //                             join d in _context.DetalleVenta on p.IdProducto equals d.IdProducto
+                //                             group p by p.Descripcion into g
+                //                             orderby g.Count() ascending
+                //                             select new DtoProductoVendidos { Producto = g.Key, Total = g.Count().ToString() })
+                //                             .Take(4)
+                //                             .ToList();
+
+                // Get the top 4 sold products in the last 30 days.
                 config.ProductosVendidos = (from p in _context.Productos
-                                             join d in _context.DetalleVenta on p.IdProducto equals d.IdProducto
-                                             group p by p.Descripcion into g
-                                             orderby g.Count() ascending
-                                             select new DtoProductoVendidos { Producto = g.Key, Total = g.Count().ToString() })
+                                            join d in _context.DetalleVenta on p.IdProducto equals d.IdProducto
+                                            join v in _context.Venta on d.IdVenta equals v.IdVenta
+                                            where v.FechaRegistro >= fecha  // Filtra por las ventas de los últimos 30 días
+                                            group p by p.Descripcion into g
+                                            orderby g.Count() descending  // Cambiado a 'descending' para obtener los más vendidos
+                                            select new DtoProductoVendidos
+                                            {
+                                                Producto = g.Key,
+                                                Total = g.Count().ToString()
+                                            })
                                              .Take(4)
                                              .ToList();
+
 
                 // Get sales count grouped by day for the last 7 days.
                 config.VentasporDias = (from v in _context.Venta
